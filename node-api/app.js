0b04de62c09551b725e51bce389514d74c52b050
João Arthur Barp Begnini <joaobarpy@gmail.com>
2025-05-13 20:53:35 -0300

    aula

---
+++ node-api/app.js
@@ -0,0 +1,15 @@
+const e = require('express');
+const express = require('express');
+const app = express();
+const host = '0.0.0.0'
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+    // axios.get
+});
+
+
+app.listen(port, host, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
